da9dfbb1ace26dd1ba298b95ff150b2fddcc0439
Ananthalakshmi <surlaanthalakshmi@gmail.com>
2026-07-14 10:09:06 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Backend Base URL
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://techoverse-fpcd.onrender.com/api/auth",
 });
 
 // ================= Register =================
